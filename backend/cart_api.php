af505e1a0511e47bddfc0eba0bac4c385c855ed8
vuthanhbinh99 <binhvuthanh483@gmail.com>
2025-12-24 00:53:19 +0700

    Update cart_api.php

--- backend/cart_api.php
+++ backend/cart_api.php
@@ -6,11 +6,10 @@
 // Cho phép truy cập từ frontend
 $allowed_origin = 'http://localhost:3000';
 
-header("Access-Control-Allow-Origin: $allowed_origin");
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 // Xử lý preflight request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
